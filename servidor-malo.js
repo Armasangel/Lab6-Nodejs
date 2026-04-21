60379b7eee2470a6ae428f2e806c34fe876320bf
Angel <angel.armasher@gmail.com>
2026-04-21 13:35:05 -0600

    The corrections identified in Part 1 were applied to the code.

--- servidor-malo.js
+++ servidor-malo.js
@@ -12,23 +12,23 @@ const server = http.createServer(async (req, res) => {
   }
 
   if (req.url === "/info") {
-    res.writeHead(200, { "Content-Type": "application-json" })
+    res.writeHead(200, { "Content-Type": "application/json" }) //CORRECION BUG1
     res.end("Ruta de información")
     return
   }
 
   if (req.url === "/api/student") {
     const filePath = path.join(process.cwd(), "datos.json")
-    const texto = fs.readFile(filePath, "utf-8")
+    const texto = await fs.readFile(filePath, "utf-8") //CORRECCION BUG2
     res.writeHead(200, { "Content-Type": "application/json" })
     res.end(JSON.stringify(texto))
     return
   }
 
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Ruta no encontrada")
-}
+  res.writeHead(404, { "Content-Type": "text/plain" }) //CORRECION BUG5
+  res.end(`Ruta no encontrada: ${req.url}`) //CORRECCION BUG3
+})
 
 server.listen(PORT, () => {
   console.log("Servidor corriendo en http://localhost:3000")
-}
+}) //CORRECION BUG4
